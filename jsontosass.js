c0886c54402553b67ad521fb1cec898f0407d700
Christian Stuff <christian.stuff@namics.com>
2016-04-19 21:53:18 +0200

    switched to semistandard

--- jsontosass.js
+++ jsontosass.js
@@ -1,205 +1,205 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    var jsontosass = function() {
-        this.nestLevel = 0;
+  var JsonToSass = function () {
+    this.nestLevel = 0;
 
-        this.defaultOptions = {
-            indent: 4, // or 'tabs'
-            prettify: true,
-            spaceAfterColon: 1,
-            spaceBeforeColon: 0,
-            syntax: 'scss',
-            useMaps: true
-        };
-
-        this.options = {};
-
-        /**
-         * Converts a given String json to Sass variables.
-         * @param {String} json The JSON string.
-         * @param {Object} options
-         * @param {String} options.indent
-         */
-        this.convert = function(json, options) {
-            var parsedJSON;
-
-            this.mergeOptions(options);
-
-            try {
-                parsedJSON = JSON.parse(json);
-            } catch(error) {
-                throw new Error('invalid json');
-            }
-            return this.convertObject(parsedJSON);
-        };
-        /**
-         * Converts a given JSON file represented by filename fileIn to a Sass file represented by fileOut.
-         * @param {String} fileIn The filename to the input JSON file.
-         * @param {String} fileOut The filename to the output Sass file.
-         */
-        this.convertFile = function(fileIn, fileOut, options) {
-            var fs = require('fs'),
-                json = fs.readFileSync(fileIn),
-                sass = this.convert(json, options);
-
-            fs.writeFileSync(fileOut, sass);
-        };
-        /**
-         * Converts a given JSON object represented by obj to a Sass map.
-         * @param {Object} obj The JSON object.
-         * @return {String} The Sass map as a string.
-         */
-        this.convertObject = function(obj) {
-            var map = [];
-
-            for(var key in obj) {
-                switch(typeof obj[key]) {
-                    case 'object':
-                        this.nestLevel++;
-                        if(this.options.useMaps || obj[key].hasOwnProperty('length')) {
-                            map.push(key + this.createColon() + this.convertObject(obj[key]));
-                        } else {
-                            map.push(key + '-' + this.convertObject(obj[key]));
-                        }
-                        this.nestLevel--;
-                        break;
-                    default:
-                        if(parseInt(key) == key) {
-                            map.push(obj[key]);
-                        } else {
-                            map.push(key + this.createColon() + obj[key]);
-                        }
-                        break;
-                }
-            }
-
-            return this.objectToString(map);
-        };
-
-        this.createIndent = function(levelAdjustment) {
-            var indent = '';
-
-            levelAdjustment = levelAdjustment || 0;
-
-            for(var l = 0; l < this.nestLevel + levelAdjustment; l++) {
-                if(typeof this.options.indent === 'number' && this.options.indent >= 0) {
-                    for(var c = 0; c < this.options.indent; c++) {
-                        indent += ' ';
-                    }
-                }
-                if(this.options.indent === 'tabs') {
-                    indent += '\t';
-                }
-            }
-            return indent;
-        };
-
-        this.createColon = function() {
-            var spaceBeforeColon = '',
-                spaceAfterColon = '',
-                i;
-
-            if(this.shouldPrettify()) {
-                for(i = 0; i < this.options.spaceBeforeColon; i++) {
-                    spaceBeforeColon += ' ';
-                }
-                for(i = 0; i < this.options.spaceAfterColon; i++) {
-                    spaceAfterColon += ' ';
-                }
-            }
-            return spaceBeforeColon + ':' + spaceAfterColon;
-        };
-
-        this.createLineSplit = function() {
-            var join ;
-
-            if(this.nestLevel > 0) {
-                join = ',';
-
-                if(this.shouldPrettifyMap()) {
-                    join += '\n' + this.createIndent();
-                }
-            } else {
-                join = ';';
-                if(this.shouldPrettify()) {
-                    join += '\n';
-                }
-                join += '$';
-            }
-            return join;
-        };
-
-        this.createObjectAfter = function(map) {
-            if(this.nestLevel > 0) {
-                var after = '';
-
-                if(this.shouldPrettifyMap()) {
-                    after += '\n' + this.createIndent(-1);
-                }
-                if(this.options.useMaps || map.length > 1) {
-                    after += ')';
-                }
-                return after;
-            } else {
-                if(this.options.syntax == 'scss') {
-                    return ';';
-                } else {
-                    return '';
-                }
-            }
-        };
-
-        this.createObjectBefore = function(map) {
-            if(this.nestLevel > 0) {
-                var before = '';
-                if(this.options.useMaps || map.length > 1) {
-                    before += '(';
-                }
-                if(this.shouldPrettifyMap()) {
-                    before += '\n' + this.createIndent();
-                }
-                return before;
-            } else {
-                return '$';
-            }
-        };
-
-        this.objectToString = function(map) {
-            return this.createObjectBefore(map) + map.join(this.createLineSplit()) + this.createObjectAfter(map);
-        };
-
-        this.mergeOptions = function(options) {
-            options = options || {};
-
-            for(var key in options) {
-                var value = this.validateOption(key, options[key]);
-                this.options[key] = value;
-            }
-        };
-
-        this.shouldPrettify = function() {
-            return (this.options.prettify || this.options.syntax == 'sass');
-        };
-
-        this.shouldPrettifyMap = function() {
-            return (this.options.prettify && this.options.useMaps) || this.options.syntax == 'sass';
-        };
-
-        this.validateOption = function(key, value) {
-            switch(key) {
-                case 'syntax':
-                    if(['sass','scss'].indexOf(value.toLowerCase()) === -1) {
-                        throw new Error('invalid value `' + value + '` for option syntax');
-                    }
-                    return value.toLowerCase();
-                default:
-                    return value;
-            }
-        };
-
-        this.mergeOptions(this.defaultOptions);
+    this.defaultOptions = {
+      indent: 4, // or 'tabs'
+      prettify: true,
+      spaceAfterColon: 1,
+      spaceBeforeColon: 0,
+      syntax: 'scss',
+      useMaps: true
     };
 
-    module.exports = new jsontosass();
+    this.options = {};
+
+    /**
+     * Converts a given String json to Sass variables.
+     * @param {String} json The JSON string.
+     * @param {Object} options
+     * @param {String} options.indent
+     */
+    this.convert = function (json, options) {
+      var parsedJSON;
+
+      this.mergeOptions(options);
+
+      try {
+        parsedJSON = JSON.parse(json);
+      } catch (error) {
+        throw new Error('invalid json');
+      }
+      return this.convertObject(parsedJSON);
+    };
+    /**
+     * Converts a given JSON file represented by filename fileIn to a Sass file represented by fileOut.
+     * @param {String} fileIn The filename to the input JSON file.
+     * @param {String} fileOut The filename to the output Sass file.
+     */
+    this.convertFile = function (fileIn, fileOut, options) {
+      var fs = require('fs');
+      var json = fs.readFileSync(fileIn);
+      var sass = this.convert(json, options);
+
+      fs.writeFileSync(fileOut, sass);
+    };
+    /**
+     * Converts a given JSON object represented by obj to a Sass map.
+     * @param {Object} obj The JSON object.
+     * @return {String} The Sass map as a string.
+     */
+    this.convertObject = function (obj) {
+      var map = [];
+
+      for (var key in obj) {
+        switch (typeof obj[key]) {
+          case 'object':
+            this.nestLevel++;
+            if (this.options.useMaps || obj[key].hasOwnProperty('length')) {
+              map.push(key + this.createColon() + this.convertObject(obj[key]));
+            } else {
+              map.push(key + '-' + this.convertObject(obj[key]));
+            }
+            this.nestLevel--;
+            break;
+          default:
+            if (key.match(/^[0-9]+$/)) {
+              map.push(obj[key]);
+            } else {
+              map.push(key + this.createColon() + obj[key]);
+            }
+            break;
+        }
+      }
+
+      return this.objectToString(map);
+    };
+
+    this.createIndent = function (levelAdjustment) {
+      var indent = '';
+
+      levelAdjustment = levelAdjustment || 0;
+
+      for (var l = 0; l < this.nestLevel + levelAdjustment; l++) {
+        if (typeof this.options.indent === 'number' && this.options.indent >= 0) {
+          for (var c = 0; c < this.options.indent; c++) {
+            indent += ' ';
+          }
+        }
+        if (this.options.indent === 'tabs') {
+          indent += '\t';
+        }
+      }
+      return indent;
+    };
+
+    this.createColon = function () {
+      var spaceBeforeColon = '';
+      var spaceAfterColon = '';
+      var i;
+
+      if (this.shouldPrettify()) {
+        for (i = 0; i < this.options.spaceBeforeColon; i++) {
+          spaceBeforeColon += ' ';
+        }
+        for (i = 0; i < this.options.spaceAfterColon; i++) {
+          spaceAfterColon += ' ';
+        }
+      }
+      return spaceBeforeColon + ':' + spaceAfterColon;
+    };
+
+    this.createLineSplit = function () {
+      var join;
+
+      if (this.nestLevel > 0) {
+        join = ',';
+
+        if (this.shouldPrettifyMap()) {
+          join += '\n' + this.createIndent();
+        }
+      } else {
+        join = ';';
+        if (this.shouldPrettify()) {
+          join += '\n';
+        }
+        join += '$';
+      }
+      return join;
+    };
+
+    this.createObjectAfter = function (map) {
+      if (this.nestLevel > 0) {
+        var after = '';
+
+        if (this.shouldPrettifyMap()) {
+          after += '\n' + this.createIndent(-1);
+        }
+        if (this.options.useMaps || map.length > 1) {
+          after += ')';
+        }
+        return after;
+      } else {
+        if (this.options.syntax === 'scss') {
+          return ';';
+        } else {
+          return '';
+        }
+      }
+    };
+
+    this.createObjectBefore = function (map) {
+      if (this.nestLevel > 0) {
+        var before = '';
+        if (this.options.useMaps || map.length > 1) {
+          before += '(';
+        }
+        if (this.shouldPrettifyMap()) {
+          before += '\n' + this.createIndent();
+        }
+        return before;
+      } else {
+        return '$';
+      }
+    };
+
+    this.objectToString = function (map) {
+      return this.createObjectBefore(map) + map.join(this.createLineSplit()) + this.createObjectAfter(map);
+    };
+
+    this.mergeOptions = function (options) {
+      options = options || {};
+
+      for (var key in options) {
+        var value = this.validateOption(key, options[key]);
+        this.options[key] = value;
+      }
+    };
+
+    this.shouldPrettify = function () {
+      return (this.options.prettify || this.options.syntax === 'sass');
+    };
+
+    this.shouldPrettifyMap = function () {
+      return (this.options.prettify && this.options.useMaps) || this.options.syntax === 'sass';
+    };
+
+    this.validateOption = function (key, value) {
+      switch (key) {
+        case 'syntax':
+          if (['sass', 'scss'].indexOf(value.toLowerCase()) === -1) {
+            throw new Error('invalid value `' + value + '` for option syntax');
+          }
+          return value.toLowerCase();
+        default:
+          return value;
+      }
+    };
+
+    this.mergeOptions(this.defaultOptions);
+  };
+
+  module.exports = new JsonToSass();
 })();
--- test/jsontosass.js
+++ test/jsontosass.js
@@ -3,178 +3,178 @@ var fs = require('fs');
 var glob = require('glob');
 var jsontosass;
 
-describe('jsontosass', function() {
-    beforeEach(function() {
-        jsontosass = require('../jsontosass.js');
-        jsontosass.mergeOptions(jsontosass.defaultOptions);
-        jsontosass.mergeOptions({
-            prettify: false
-        });
+describe('jsontosass', function () {
+  beforeEach(function () {
+    jsontosass = require('../jsontosass.js');
+    jsontosass.mergeOptions(jsontosass.defaultOptions);
+    jsontosass.mergeOptions({
+      prettify: false
     });
-    after(function() {
-        // delete generated test sass files
-        glob('test/*.s[ac]ss', function(er, files) {
-            files.forEach(function(file) {
-                fs.unlink(file);
-            });
-        });
+  });
+  after(function () {
+    // delete generated test sass files
+    glob('test/*.s[ac]ss', function (er, files) {
+      files.forEach(function (file) {
+        fs.unlink(file);
+      });
     });
-    describe('module', function() {
-        it('is present', function() {
-            assert.isDefined(jsontosass, 'jsontosass is defined');
-        });
-        it('is an object', function() {
-            assert.isObject(jsontosass, 'jsontosass is an object');
-        });
+  });
+  describe('module', function () {
+    it('is present', function () {
+      assert.isDefined(jsontosass, 'jsontosass is defined');
     });
-    describe('createColon()', function() {
-        it('creates configured spaces before colon', function() {
-            jsontosass.mergeOptions({
-                prettify: true,
-                spaceBeforeColon: 4
-            });
-            assert.equal(jsontosass.createColon(), '    : ');
-        });
-        it('creates configured spaces after colon', function() {
-            jsontosass.mergeOptions({
-                prettify: true,
-                spaceAfterColon: 4
-            });
-            assert.equal(jsontosass.createColon(), ':    ');
-        });
+    it('is an object', function () {
+      assert.isObject(jsontosass, 'jsontosass is an object');
     });
-    describe('convertObject()', function() {
-        it('is present', function() {
-            assert.isFunction(jsontosass.convertObject);
-        });
-        it('creates Sass variable named $key on root level', function() {
-            assert.equal(jsontosass.convertObject({
-                key: 'value'
-            }), '$key:value;');
-        });
-        it('creates Sass variables named by JSON key for multiple entries', function() {
-            assert.equal(jsontosass.convertObject({
-                key: 'value',
-                key2: 'otherValue'
-            }), '$key:value;$key2:otherValue;');
-        });
-        it('creates Sass map for nested object', function() {
-            assert.equal(jsontosass.convertObject({
-                key: {
-                    key2: 'value'
-                }
-            }), '$key:(key2:value);');
-        });
-        it('creates Sass list for JSON array', function() {
-            assert.equal(jsontosass.convertObject({
-                key: [1,2,3]
-            }), '$key:(1,2,3);');
-        });
+  });
+  describe('createColon()', function () {
+    it('creates configured spaces before colon', function () {
+      jsontosass.mergeOptions({
+        prettify: true,
+        spaceBeforeColon: 4
+      });
+      assert.equal(jsontosass.createColon(), '    : ');
     });
-    describe('convertFile()', function() {
-        it('is present', function() {
-            assert.isFunction(jsontosass.convertFile);
-        });
-        it('basic file conversion', function() {
-            var sass;
-            jsontosass.convertFile('test/basic.json', 'test/basic.scss');
-            sass = fs.readFileSync('test/basic.scss');
-            assert.equal(sass, '$key:value;');
-        });
-        it('extended file conversion', function() {
-            var sass;
-            jsontosass.convertFile('test/extended.json', 'test/extended.scss');
-            sass = fs.readFileSync('test/extended.scss');
-            assert.equal(sass, '$key:(inner-key:(1,2,3),some-object:(color-black:#000,font-family:\'Helvetica, sans-serif\'));');
-        });
+    it('creates configured spaces after colon', function () {
+      jsontosass.mergeOptions({
+        prettify: true,
+        spaceAfterColon: 4
+      });
+      assert.equal(jsontosass.createColon(), ':    ');
     });
-    describe('convert()', function() {
-        it('is present', function() {
-            assert.isFunction(jsontosass.convert);
-        });
-        it('throws an error when invalid JSON is given', function() {
-            assert.throws(function() {
-                jsontosass.convert('');
-            }, Error);
-        });
-        it('returns a string', function() {
-            assert.typeOf(jsontosass.convert('{}'), 'string');
-        });
-        it('returns Sass variables in own line when prettify is enabled', function() {
-            assert.equal(jsontosass.convert('{"key":"value","key2":"otherValue"}', {
-                prettify: true
-            }), '$key: value;\n$key2: otherValue;');
-        });
-        it('return newLines for map variables', function() {
-            assert.equal(jsontosass.convert('{"key":{"key2":"value"},"key2":"otherValue"}', {
-                prettify: true
-            }), '$key: (\n    key2: value\n);\n$key2: otherValue;');
-        });
-        it('respect default indent', function() {
-            assert.equal(jsontosass.convert('{"key":{"key2":"value"}}', {
-                prettify: true
-            }), '$key: (\n    key2: value\n);');
-        });
-        it('respect indent with tabs', function() {
-            assert.equal(jsontosass.convert('{"key":{"key2":"value"}}', {
-                indent: 'tabs',
-                prettify: true
-            }), '$key: (\n\tkey2: value\n);');
-            assert.equal(jsontosass.convert('{"key":{"key2":{"key3":"value"}}}'), '$key: (\n\tkey2: (\n\t\tkey3: value\n\t)\n);');
-        });
-        it('generate dashed variables instead of maps', function() {
-            assert.equal(jsontosass.convert('{"key":{"key2":"value"}}', {
-                indent: 'tabs',
-                prettify: true,
-                useMaps: false
-            }), '$key-key2: value;');
-            assert.equal(jsontosass.convert('{"key":{"key2":{"key3":"value"}}}'), '$key-key2-key3: value;');
-            assert.equal(jsontosass.convert('{"key":{"key2":{"key3":[1,2,3]}}}'), '$key-key2-key3: (1,2,3);');
-        });
+  });
+  describe('convertObject()', function () {
+    it('is present', function () {
+      assert.isFunction(jsontosass.convertObject);
     });
-    describe('options', function() {
-        it('false syntax option should throw an error', function() {
-            assert.throws(function() {
-                jsontosass.convert('{"key":"value"}', {
-                    syntax: 'wrong-syntax'
-                });
-            }, Error);
-        });
-        it('right syntax option but with capital letters should not throw an error', function() {
-            assert.doesNotThrow(function() {
-                jsontosass.convert('{"key":"value"}', {
-                    syntax: 'SCSS'
-                });
-            }, Error);
-        });
+    it('creates Sass variable named $key on root level', function () {
+      assert.equal(jsontosass.convertObject({
+        key: 'value'
+      }), '$key:value;');
     });
-    describe('Sass syntax', function() {
-        it('no semicolon in output', function() {
-            assert.isNull(jsontosass.convert('{"key":"value"}', {
-                syntax: 'sass'
-            }).match(/;/g));
-        });
-        it('setting prettify to false should prettify anyway', function() {
-            assert.equal(jsontosass.convert('{"key":{"key2":"value"}}', {
-                syntax: 'sass'
-            }), '$key: (\n    key2: value\n)');
-        });
-        it('basic file conversion', function() {
-            var sass;
-            jsontosass.convertFile('test/basic.json', 'test/basic.sass', {
-                syntax: 'sass'
-            });
-            sass = fs.readFileSync('test/basic.sass');
-            assert.equal(sass, '$key: value');
-        });
-        it('extended file conversion', function() {
-            var sass;
-            jsontosass.convertFile('test/extended.json', 'test/extended.sass', {
-                indent: 'tabs',
-                syntax: 'sass'
-            });
-            sass = fs.readFileSync('test/extended.sass');
-            assert.equal(sass, '$key: (\n\tinner-key: (\n\t\t1,\n\t\t2,\n\t\t3\n\t),\n\tsome-object: (\n\t\tcolor-black: #000,\n\t\tfont-family: \'Helvetica, sans-serif\'\n\t)\n)');
-        });
+    it('creates Sass variables named by JSON key for multiple entries', function () {
+      assert.equal(jsontosass.convertObject({
+        key: 'value',
+        key2: 'otherValue'
+      }), '$key:value;$key2:otherValue;');
     });
+    it('creates Sass map for nested object', function () {
+      assert.equal(jsontosass.convertObject({
+        key: {
+          key2: 'value'
+        }
+      }), '$key:(key2:value);');
+    });
+    it('creates Sass list for JSON array', function () {
+      assert.equal(jsontosass.convertObject({
+        key: [1, 2, 3]
+      }), '$key:(1,2,3);');
+    });
+  });
+  describe('convertFile()', function () {
+    it('is present', function () {
+      assert.isFunction(jsontosass.convertFile);
+    });
+    it('basic file conversion', function () {
+      var sass;
+      jsontosass.convertFile('test/basic.json', 'test/basic.scss');
+      sass = fs.readFileSync('test/basic.scss');
+      assert.equal(sass, '$key:value;');
+    });
+    it('extended file conversion', function () {
+      var sass;
+      jsontosass.convertFile('test/extended.json', 'test/extended.scss');
+      sass = fs.readFileSync('test/extended.scss');
+      assert.equal(sass, "$key:(inner-key:(1,2,3),some-object:(color-black:#000,font-family:'Helvetica, sans-serif'));");
+    });
+  });
+  describe('convert()', function () {
+    it('is present', function () {
+      assert.isFunction(jsontosass.convert);
+    });
+    it('throws an error when invalid JSON is given', function () {
+      assert.throws(function () {
+        jsontosass.convert('');
+      }, Error);
+    });
+    it('returns a string', function () {
+      assert.typeOf(jsontosass.convert('{}'), 'string');
+    });
+    it('returns Sass variables in own line when prettify is enabled', function () {
+      assert.equal(jsontosass.convert('{"key":"value","key2":"otherValue"}', {
+        prettify: true
+      }), '$key: value;\n$key2: otherValue;');
+    });
+    it('return newLines for map variables', function () {
+      assert.equal(jsontosass.convert('{"key":{"key2":"value"},"key2":"otherValue"}', {
+        prettify: true
+      }), '$key: (\n    key2: value\n);\n$key2: otherValue;');
+    });
+    it('respect default indent', function () {
+      assert.equal(jsontosass.convert('{"key":{"key2":"value"}}', {
+        prettify: true
+      }), '$key: (\n    key2: value\n);');
+    });
+    it('respect indent with tabs', function () {
+      assert.equal(jsontosass.convert('{"key":{"key2":"value"}}', {
+        indent: 'tabs',
+        prettify: true
+      }), '$key: (\n\tkey2: value\n);');
+      assert.equal(jsontosass.convert('{"key":{"key2":{"key3":"value"}}}'), '$key: (\n\tkey2: (\n\t\tkey3: value\n\t)\n);');
+    });
+    it('generate dashed variables instead of maps', function () {
+      assert.equal(jsontosass.convert('{"key":{"key2":"value"}}', {
+        indent: 'tabs',
+        prettify: true,
+        useMaps: false
+      }), '$key-key2: value;');
+      assert.equal(jsontosass.convert('{"key":{"key2":{"key3":"value"}}}'), '$key-key2-key3: value;');
+      assert.equal(jsontosass.convert('{"key":{"key2":{"key3":[1,2,3]}}}'), '$key-key2-key3: (1,2,3);');
+    });
+  });
+  describe('options', function () {
+    it('false syntax option should throw an error', function () {
+      assert.throws(function () {
+        jsontosass.convert('{"key":"value"}', {
+          syntax: 'wrong-syntax'
+        });
+      }, Error);
+    });
+    it('right syntax option but with capital letters should not throw an error', function () {
+      assert.doesNotThrow(function () {
+        jsontosass.convert('{"key":"value"}', {
+          syntax: 'SCSS'
+        });
+      }, Error);
+    });
+  });
+  describe('Sass syntax', function () {
+    it('no semicolon in output', function () {
+      assert.isNull(jsontosass.convert('{"key":"value"}', {
+        syntax: 'sass'
+      }).match(/;/g));
+    });
+    it('setting prettify to false should prettify anyway', function () {
+      assert.equal(jsontosass.convert('{"key":{"key2":"value"}}', {
+        syntax: 'sass'
+      }), '$key: (\n    key2: value\n)');
+    });
+    it('basic file conversion', function () {
+      var sass;
+      jsontosass.convertFile('test/basic.json', 'test/basic.sass', {
+        syntax: 'sass'
+      });
+      sass = fs.readFileSync('test/basic.sass');
+      assert.equal(sass, '$key: value');
+    });
+    it('extended file conversion', function () {
+      var sass;
+      jsontosass.convertFile('test/extended.json', 'test/extended.sass', {
+        indent: 'tabs',
+        syntax: 'sass'
+      });
+      sass = fs.readFileSync('test/extended.sass');
+      assert.equal(sass, "$key: (\n\tinner-key: (\n\t\t1,\n\t\t2,\n\t\t3\n\t),\n\tsome-object: (\n\t\tcolor-black: #000,\n\t\tfont-family: 'Helvetica, sans-serif'\n\t)\n)");
+    });
+  });
 });
